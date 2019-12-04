5b2e04b817d466f040ab503e116bb123e36cf781
Justin Johnson <authalic@gmail.com>
2019-12-04 13:33:43 -0700

    reformatted, cleanup, comments, tested, working

--- EDD_requests.py
+++ EDD_requests.py
@@ -7,13 +7,14 @@ from json import JSONDecodeError
 """
 Noxious Weed Data Client
 
-Obtains records of noxious plant species from EDDMapS using API data requests.
-Allows export of data in a GIS-friendly format.
-
-Data Sources and Documentation:
-Early Detection & Distribution Mapping System:  https://www.eddmaps.org/
-API Documentation: https://developers.bugwood.org/
+Obtains records of a noxious plant species from EDDMapS using API data requests.
+Provides methods to export that data in native JSON and a GIS-friendly GeoJSON format.
 """
+
+# Data Sources and Documentation:
+# Early Detection & Distribution Mapping System:  https://www.eddmaps.org/
+# API Documentation: https://developers.bugwood.org/
+
 
 # State of Utah Noxious Weed List
 # source:  https://ag.utah.gov/farmers/plants-industry/noxious-weed-control-resources/state-of-utah-noxious-weed-list/
@@ -78,8 +79,9 @@ noxweeds = [
 
 
 # list of counties for data request
-countyfips = [49003, 49005, 49011, 49023, 49029, 49035, 49039, 49043, 49045, 49049, 49051, 49057]
+countyFIPS = [49003, 49005, 49011, 49023, 49029, 49035, 49039, 49043, 49045, 49049, 49051, 49057]
 
+# 5-digit county FIPS codes:
 # 49003 - Box Elder
 # 49005 - Cache
 # 49011 - Davis *
@@ -93,40 +95,38 @@ countyfips = [49003, 49005, 49011, 49023, 49029, 49035, 49039, 49043, 49045, 490
 # 49051 - Wasatch *
 # 49057 - Weber *
 
-# API Data Request limits:
-# Current limit for records returned per request is 3000
 
-    # Option 1
-    # url.start - this is the starting record number.
-    # url.length - this is how many records to return.
-
-    # Option2
-    # url.rows - number of records to return as a "page".
-    # url.page - page number to return.
-
-
-# EDD Occurrences
+# Get EDD Occurrences from API
 # API Documentation:  https://developers.bugwood.org/Occurrences/
+# Subject ID lookup:  http://maphelp.eddmaps.org/basics/subject-lookup/
+
 url = 'https://api.bugwood.org/rest/api/occurrence.json'
 
+# payload is a dict of key/value pairs, sent as URL querystring
 payload = {
-    'includeonly': 'objectid,SubjectNumber,commonname,WellKnownText,InfestationStatus,ObservationDate,reporter,Density',
+    'includeonly': 'objectid,SubjectNumber,commonname,WellKnownText,InfestationStatus,ObservationDate,reporter,Density', #fieldnames
     'sub': 0,  # EDD Subject ID (plant species)
-    'dateFormat': 101,  # SQL Server date format code: mm/dd/yyyy
-    'countyfips': countyfips,
-    'start': 0,
-    'length': 1
+    'dateFormat': 101,  # SQL Server date format code: mm/dd/yyyy 
+    'countyfips': countyFIPS, # list of county FIPS numbers
+    'start': 0, # first record in request
+    'length': 1 # number of records in request (3000 limit)
     }
 
-# User Agent header
-# Signature requested by Bugwood maintainers to track/contact API users
+# User-Agent header
+# Requested by Bugwood maintainers to track/contact API users
 headers = {'user-agent': 'Justin Johnson, Utah DNR, jjohnson2@utah.gov'}
 
 
 class Occurrence:
+    """
+    Records of an invasive species occurrence from:
+    Early Detection and Distribution Mapping System (EDDMapS)
+    Center for Invasive Species and Ecosystems Health
+    University of Georgia
+    """
 
     def getRecords(self, speciescode):
-        """returns a dict containing all records for the species code"""
+        """returns a dict from BugwoodAPI containing all records for the species code"""
 
         payload["sub"] = speciescode
         recordcount = self.recordsTotal  # total record count in feature service
@@ -160,8 +160,13 @@ class Occurrence:
 
 
     def __init__(self, speciescode):
+        """
+        Constructor:
+        Requires a numeric Subject ID code for the desired plant species
+        Subject ID lookup:  http://maphelp.eddmaps.org/basics/subject-lookup/
+        """
 
-        self.subject = str(speciescode)  # species code from Bugwood API subjectnumber
+        self.subject = str(speciescode)  # species code from Bugwood API subject id number
 
         # update the species code in the API request payload
         payload["sub"] = speciescode
@@ -169,7 +174,7 @@ class Occurrence:
         # request a single record to get the "recordsTotal" value for species
         r = requests.get(url, headers=headers, params=payload)
 
-        # save record count as object attribute
+        # save record count as attribute, to be used later in requesting all records
         self.recordsTotal = json.loads(r.text)["recordsTotal"]
 
         # send the request to get all records for species code
@@ -179,6 +184,7 @@ class Occurrence:
 
     def __repr__(self):
         """print the common name and scientific name of species from noxweeds list"""
+
         for x in noxweeds:
             if x['sub'] == self.subject:
                 return f"{x['name']} ({x['sciname']})"
@@ -186,102 +192,124 @@ class Occurrence:
 
     def count(self):
         """Returns the number of records in the EDDMapS database for this species code"""
+
         return self.recordsTotal
 
 
-    def filterspatial(self):
-        """Removes any records that have empty strings in the wellknowntext field"""
-        pass
+    def as_JSON(self):
+        """Returns the EDDMapS species records as JSON
+        No changes or corrections are made to the data returned from the API
+        Errors may exist in spatial data.
+        To export for geospatial use, use as_GeoJSON()"""
 
-
-    def as_json(self):
-        """Returns the species records as JSON"""
         return json.dumps(self.records)
 
 
-    def as_jsonp(self):
-        """Returns the species records as JSON formatted for viewing"""
-        return json.dumps(self.records, sort_keys=True, indent=4)
-
-
-    def export(self, filename):
+    def export_JSON(self, filename):
         """Exports records to a JSON file"""
+
         with open(filename, 'w') as file:
-            file.write(self.as_json())
+            file.write(self.as_JSON())
 
 
-    def export_p(self, filename):
-        """Exports a prettified JSON file, for viewing"""
+    def as_JSONPretty(self):
+        """Returns the species records as JSONP formatted for human readability"""
+
+        return json.dumps(self.records, sort_keys=False, indent=4)
+
+
+    def export_JSONPretty(self, filename):
+        """Exports a prettified JSON file, formatted for human readability
+        Note: File sizes will be larger when JSONPretty() is used. 
+        If formatting is not required use the export_JSON() method"""
+
         with open(filename, 'w') as file:
-            file.write(self.as_jsonp())
+            file.write(self.as_JSONPretty())
 
 
-# Usage
-# import this file as a module
-# instantiate a new Occurrence object, using the subjectnumber ('sub' value in the noxweeds list)
+    def reformat(self, geom):
+        """Helper function: changes coordinate pair string(s) from WKT to GeoJSON
+        Don't call directly. Use as_GeoJSON()"""
 
+        # Split the WKT geometry string into a tuple of string fields
+        # (geometry type, geometry coordinates)
+        #  NOTE: input data from EDDMapS is not consistently and properly formatted
+        #  eg. there may, or may not, be a space between geom and ()
+        #      coordinates have been found with numbers outside parentheses
+        # some string hacks are required in order to get data into usable format
+        # records that cannot be rescued are skipped
 
-
-
-# TESTCODE
-
-if __name__ == "__main__":
-    # o = Occurrence(3937)
-    # print(o.count())
-    # print(o.as_jsonp())
-    # o.export_p("test.json")
-
-    def get_from_file(filename):
-        """
-        Open an existing JSON file into a dict with the same format as the EDD results
-        reverse of: export(self, filename)
-        """
-
-        with open(filename, 'r') as file:
-            records = file.read()
+        # geom is the WKT geometry
+        # examples: "POINT(-112.213747 41.892975)", "POINT (-111.9223893 40.6292995)"
         
-        return json.loads(records)
-
-
-    def reformat(geom):
-        """Helper function: change coordinate pair string from WKT to GeoJSON"""
-
-        # Split the WKT geometry string into a geometry type, and the coordinates
-        #  input data is not consistently formatted. some string hacks are required
-        #  there may, or may not, be a space between geom and ()
-
+        # split at first parenthesis
         g = geom.split( '(', maxsplit=1 )
-        geomtype = g[0].strip().title() #remove any extra spaces and convert case to title
 
-        # NOTE: the correct spelling of "Linestring" is "LineString"
+        # get the geometry type (Point, LineString, Polygon)
+        # strip any extra spaces and convert case to title
+        geomtype = g[0].strip().title() 
+
+        # NOTE: the correct spelling of "Linestring" in GeoJSON spec is "LineString"
+        # alternate spelling appears in some EDDMapS records
         if geomtype == "Linestring":
             geomtype = "LineString"
 
-        coordstr = '(' + g[1] # restore opening paren which was lost splitting string
+        # restore opening parenthesis, which was lost splitting string earlier #ugly
+        coords = '(' + g[1]
 
-        # RE matches lat/lon coordinate strings, like: "-111.941542 41.2621659"
-        c = re.compile(r'-\d+.\d+\s\d+.\d+')  #NW hemisphere only
+        # RE matches lat/lon coordinate strings, with the format: "-111.9415 41.26219"
+        c = re.compile(r'-\d+.\d+\s\d+.\d+')  #NW global quadrant coordinates only
 
-        # get a list of coordinate pairs
-        coordlist = c.findall(coordstr)
+        # get a list of coordinate pairs in the current geometry using the RE module
+        # Point has a single pair, LineString and Polygon will have more
+        coordlist = c.findall(coords)
 
         # change the format of the coordinate pairs
-        #   from:  "-111.941542 41.2621659,"     <-- WKT
-        #     to:  "[-111.941542, 41.2621659],"  <-- GeoJSON
+        #   from:  "-111.941542 41.2621659"     <-- WKT
+        #     to:  "[-111.941542, 41.2621659]"  <-- GeoJSON
         for coord in coordlist:
+            # replace whitespace(s) delimeter with a comma
+            # add square brackets around coordinate pair
+            newcoord = '[' + re.sub(r'[ ]+', ', ', coord) + ']'
+            coords = coords.replace(coord, newcoord)
 
-            newcoord = '[' + coord.replace(' ', ', ') + ']' # add squre brackets and comma
-            coordstr = coordstr.replace(coord, newcoord)
+            #TODO:  rename the coords variable, since it contains a list below
 
-        # replace all parentheses with square brackets
-        coordstr = coordstr.replace('(', '[')
-        coordstr = coordstr.replace(')', ']')
-
-        return (geomtype, coordstr)
+        # replace all remaining parentheses in coordinate string with square brackets
+        coords = coords.replace('(', '[')
+        coords = coords.replace(')', ']')
 
 
+        # convert the coordinates from a string to a JSON array (Python list)
+        # Point features seem to add extra square brackets: [[-111.941542, 41.2621659]]
 
-    def to_GeoJSON(records):
+        # NOTE: The following if..else is an ugly workaround
+        # TODO: find out where the extra brackets are coming from
+
+        # ERROR in WKT
+        # Apparently, bad WKT can be uploaded to EDDMapS through their web portal
+        # check for JSONDecodeError when converting from WKT string to dict using json.loads()
+
+        try:  
+            JSONgeom = json.loads(coords)
+            # at this point, the coordinate string is a Python dict
+
+            if geomtype == "Point":
+                # the point is stored in a list inside a list: [[lon, lat]]
+                # extract the inner list 
+                coords = JSONgeom[0] 
+            else:
+                coords = JSONgeom
+            
+        except JSONDecodeError:
+            # badly formatted WKT... skip it
+            pass
+
+        # return a tuple containing reformatted (for GeoJSON) geometry type and coordinates
+        return (geomtype, coords)
+
+
+    def as_GeoJSON(self):
         """
         Converts a dict of species records to properly formatted GeoJSON string
         for input into QGIS or ArcGIS
@@ -291,7 +319,9 @@ if __name__ == "__main__":
 
         # Geometries in EED Data
         # stored as WKT
-        # 3 geometry types found (so far): {'POINT', 'POLYGON', 'LINESTRING'}
+        # 3 geometry types found (so far): ('POINT', 'POLYGON', 'LINESTRING')
+        # if another geom is used in the EDDMapS, ie 'MULTIPOINT' or 'MULTIPOLYGON',
+        # it should work in the code below, but hasn't been tested
 
         # GeoJSON FeatureCollection
         # dict containing all of the records reformatted for GeoJSON dump
@@ -300,25 +330,22 @@ if __name__ == "__main__":
             "features": []
         }
 
-        # list of columns returned from API
-        cols = records["columns"]
+        # list of columns returned from API, should match the list sent in API request
+        cols = self.records["columns"]
 
-        # list of records
-        # each item in the list is a value for column at that position
-        data = records["data"]
+        # extract list of records from dict
+        data = self.records["data"]
 
         for record in data:
-
-            # create a dict of column names for key/value pairs
-            # NOTE: the wellknowntext field can be dropped from the properties after it is
-            #       converted to GeoJSON (below)
+            # create a dict of column names for key/value pairs, for GeoJSON format
             properties = {col: "" for col in cols}
 
+            # for each record, populate the properties dict with values from record
             for x in range(len(cols)):
                 # use 'range' to maintain order of matching between cols and record
                 properties[cols[x]] = record[x]
 
-            # new feature dict
+            # new feature dict for current record (see GeoJSON format specification)
             feat = {
                 "type": "Feature", 
                 "geometry": {
@@ -327,58 +354,51 @@ if __name__ == "__main__":
                     }, 
                 "properties": properties
                 }
-
             
-            #TODO: Convert the value stored in "wellknowntext" from WKT to GeoJSON
-            #      and store that in the feature "geometry" dict
+            # populate the value of the feat["geometry"["coordinates"] key
+            # with a list of coordinates
 
             if 'wellknowntext' in cols:
+                # there is a wellknowntext attribute in the data returned from EDDMapS
+
+                # NOTE: some values returned from EDDMapS do not contain anything in the
+                # wellknowntext field, and cannot be mapped. They are skipped here.
 
                 if feat["properties"]["wellknowntext"]:
+                    # the wellknowntext attribute is not an empty string
+
                     wkt = feat["properties"]["wellknowntext"]
-                    geofmts = reformat(wkt)
 
-                    feat["geometry"]["type"] = geofmts[0] #Geometry type
+                    # send the WKT geometry to be reformatted into GeoJSON
+                    geofmts = self.reformat(wkt)
 
-                    # convert the coordinates from a string to a JSON array (Python list)
-                    # Point features seem to add extra square brackets [[ x,y ]]
-                    # NOTE: The following if..else is a workaround
+                    feat["geometry"]["type"] = geofmts[0] # Geometry type string
+                    feat["geometry"]["coordinates"] = geofmts[1] # Geometry coordinate list
 
-                    # ERROR in WKT
-                    # Apparently, bad WKT can be uploaded to EDD
-                    # check for JSONDecodeError when converting from WKT string to JSON
+                    # remove the old WKT property from the properties dict
+                    # it is now stored in the geometry dict
+                    properties.pop("wellknowntext")
 
-                    try:
-                        jsongeom = json.loads(geofmts[1])
-
-                        if feat["geometry"]["type"] == "Point":
-                            feat["geometry"]["coordinates"] = jsongeom[0]
-                        else:
-                            feat["geometry"]["coordinates"] = jsongeom
-
-                        # remove the old WKT property
-                        properties.pop("wellknowntext")
-
-                        # append the new feature to the features list in the FeatureCollection dict
-                        collection["features"].append(feat)
-                        
-                    except JSONDecodeError:
-                        # badly formatted WKT... skip it
-                        pass
-
-                else:
-                    # no spatial geometry exists... skip it
-                    pass
-
+                    # append the current feature to the features list in the FeatureCollection dict
+                    collection["features"].append(feat)
 
         return json.dumps(collection, indent=4)
 
-    x = get_from_file('test.json')
 
-    # print(to_GeoJSON(x))
-    to_GeoJSON(x)
+    def export_GeoJSON(self, filename):
+        """Exports records to a GeoJSON file"""
+
+        with open(filename, 'w') as file:
+            file.write(self.as_GeoJSON())
 
 
+# TESTCODE
 
-# TODO:  the value of the "coordinates" value under the "geometry" object needs to be a javascript array, not a string
-#        also, the point array has two brackets on each end. Remove one pair, for points only.
+if __name__ == "__main__":
+    o = Occurrence(4411) # 4411 - St. Johnswort
+    print(o.count())
+    
+
+    o.export_JSON("test1.json")
+    o.export_JSONPretty("test1_pretty.json")
+    o.export_GeoJSON("test1_geo.json")
